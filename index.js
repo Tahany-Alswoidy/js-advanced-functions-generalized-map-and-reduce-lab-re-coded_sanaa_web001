bc07a0b5f7032d74214628198b8f319c1e90f2b6
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-13 06:41:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 function map(sourceArray,myfunction){
   return sourceArray.map(a=>myfunction(a));
 }
-function reduce(sourceArray,myfunction){
+function reduce(sourceArray,total,totalwithstart,alltrue,anytrue,starting){
+  if(starting){return }
   return sourceArray.reduce(a=>myfunction(a,memo));
 }
